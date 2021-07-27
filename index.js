da9d97473ce85c8504b51009e8d8b56e10d64c07
Tyler Bray <broncobray77@gmail.com>
2021-07-27 11:46:55 -0700

    api request

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+var searchbutton = document.getElementById("search")
+
+function currentWeather(){
+
+  var city = document.querySelector('.form-control').value
+  console.log(city)
+
+
+  var url  = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c127f37b11a1d9bfa4118867b07f57a7`
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  });
+}
+
+searchbutton.addEventListener('click', currentWeather)
