10e829bb515c55ab22afe90bfdba8910814a5098
Tyler Bray <broncobray77@gmail.com>
2021-07-28 14:13:56 -0700

    update

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function currentWeather(){
   var city = document.querySelector('.form-control').value
   console.log(city)
 
-
   var url  = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=c127f37b11a1d9bfa4118867b07f57a7`
 
   fetch(url)
